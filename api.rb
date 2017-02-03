737c96e29df0b0831699e3d37a2c2f228920d69b
Bryce Darling <bryce@theironyard.com>
2017-02-03 12:25:37 -0500

    Updates

--- api.rb
+++ api.rb
@@ -2,9 +2,13 @@ require 'sinatra'
 require 'active_record'
 require 'yaml'
 
-database_config = YAML::load(File.open('config/database.yml'))
+database_config = ENV['DATABASE_URL']
 
-ActiveRecord::Base.establish_connection(database_config)
+if database_config.blank?
+  database_config = YAML::load(File.open('config/database.yml'))
+end
+
+ActiveRecord::Base.establish_connection(database_url)
 
 after do
   ActiveRecord::Base.connection.close
@@ -14,4 +18,3 @@ get '/api/greet/:name?' do
   name = params[:name] || 'world'
   "Hello, #{name}!"
 end
-
