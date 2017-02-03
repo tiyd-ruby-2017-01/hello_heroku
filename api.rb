bce3b7985c9c738743c4e46aa2d34b1c036852bb
Bryce Darling <bryce@theironyard.com>
2017-02-03 12:30:55 -0500

    Updated API

--- api.rb
+++ api.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'active_record'
 require 'yaml'
+require_relative 'models/weapon'
 
 database_config = ENV['DATABASE_URL']
 
@@ -17,4 +18,26 @@ end
 get '/api/greet/:name?' do
   name = params[:name] || 'world'
   "Hello, #{name}!"
+end
+
+get '/api/weapons' do
+  Weapon.all.to_json
+end
+
+post '/api/weapons' do
+  status 201
+  Weapon.create(params).to_json
+end
+
+patch '/api/weapons/:id' do |id|
+  weapon = Weapon.find_by_id(id)
+
+  halt 404 if weapon.nil?
+
+  params.delete('splat')
+  params.delete('captures')
+
+  weapon.update(params)
+
+  weapon.to_json
 end
